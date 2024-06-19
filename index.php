0935bf57fdcddc737ee9b272e9bade84f253e8ea
dalmazanv <150323753+dalmazanv@users.noreply.github.com>
2024-06-19 18:30:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 // Nombre de la cuenta de Azure Storage
 $accountName = 'filestestapi';
-// Clave de la cuenta de Azure Storage
-$accountKey = 'YQncsNA6UrxbjruMaUvfVHXrvwbWkbT7Pv73/+csgUgqdkOrcLCT0RbPLRTAlWku5SUJej9Rib8P+ASt23rOdg';
+// Clave de la cuenta de Azure Storage (en base64)
+$accountKey = 'YQncsNA6UrxbjruMaUvfVHXrvwbWkbT7Pv73/+csgUgqdkOrcLCT0RbPLRTAlWku5SUJej9Rib8P+ASt23rOdg==';
 
 // URL del archivo en Azure Files
 $url = 'https://filestestapi.file.core.windows.net/filesharetestapi/hola.txt';
@@ -10,11 +10,19 @@ $url = 'https://filestestapi.file.core.windows.net/filesharetestapi/hola.txt';
 // Inicializa cURL
 $ch = curl_init($url);
 
-// Genera el valor de autorización
+// Genera la fecha en formato GMT
 $date = gmdate('D, d M Y H:i:s T');
-$headers = "x-ms-date:$date\nx-ms-version:$version";
-$signatureString = "GET\n\n\n\n\n\n\n\n\n\n\n\n\n$headers\n/$accountName/filesharetestapi/hola.txt";
-$signature = base64_encode(hash_hmac('sha256', $signatureString, base64_decode($accountKey), true));
+
+// Versión de la API de Azure
+$version = '2021-06-08'; // Puedes probar con otras versiones si esta no funciona
+
+// StringToSign
+$stringToSign = "GET\n\n\n\n\n\n\n\n\n\n\n\n\nx-ms-date:$date\nx-ms-version:$version\n/$accountName/filesharetestapi/hola.txt";
+
+// Calcula la firma HMAC-SHA256
+$signature = base64_encode(hash_hmac('sha256', $stringToSign, base64_decode($accountKey), true));
+
+// Cabecera de autorización
 $authHeader = "Authorization: SharedKey $accountName:$signature";
 
 // Configura las opciones de cURL para obtener el contenido del archivo
@@ -23,7 +31,8 @@ curl_setopt($ch, CURLOPT_HEADER, false);
 
 // Añade los encabezados necesarios
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-   "x-ms-date: $date",
+    "x-ms-date: $date",
+    "x-ms-version: $version",
     $authHeader
 ));
 
@@ -31,7 +40,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $contenido = curl_exec($ch);
 
 // Verifica si hubo un error
-if (curl_errno($ch)) {
+if(curl_errno($ch)) {
     echo 'Error de cURL: ' . curl_error($ch);
 } else {
     // Muestra el contenido del archivo
